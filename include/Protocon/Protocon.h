b9400096f3ac830a39f4dc156bb29f706c53e3ba
Bomou He <paakmau@outlook.com>
2021-10-07 01:21:11 +0800

    添加了 Engine 与 EngineBuilder

--- include/Protocon/Protocon.h
+++ include/Protocon/Protocon.h
@@ -51,4 +51,37 @@ class ResponseHandler {
     virtual void handle(const ReceivedResponse& response) = 0;
 };
 
+class Engine {
+  private:
+    Engine(uint16_t apiVersion,
+           std::vector<std::unique_ptr<RequestHandler>>&& requestHandlers,
+           std::vector<std::unique_ptr<ResponseHandler>>&& responseHandlers)
+        : apiVersion(apiVersion), requestHandlers(std::move(requestHandlers)), responseHandlers(std::move(responseHandlers)) {}
+
+    uint16_t apiVersion;
+    std::vector<std::unique_ptr<RequestHandler>> requestHandlers;
+    std::vector<std::unique_ptr<ResponseHandler>> responseHandlers;
+
+    friend class EngineBuilder;
+};
+
+class EngineBuilder {
+  public:
+    EngineBuilder(uint16_t apiVersion) : apiVersion(apiVersion) {}
+    EngineBuilder& with(std::unique_ptr<RequestHandler>&& handler) {
+        requestHandlers.emplace_back(std::move(handler));
+        return *this;
+    }
+    EngineBuilder& with(std::unique_ptr<ResponseHandler>&& handler) {
+        responseHandlers.emplace_back(std::move(handler));
+        return *this;
+    }
+    Engine build() { return Engine(apiVersion, std::move(requestHandlers), std::move(responseHandlers)); }
+
+  private:
+    uint16_t apiVersion;
+    std::vector<std::unique_ptr<RequestHandler>> requestHandlers;
+    std::vector<std::unique_ptr<ResponseHandler>> responseHandlers;
+};
+
 }  // namespace Protocon

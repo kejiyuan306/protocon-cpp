ec7eca838d6b13f6c44a856b7f9b5ec202cd02fb
Bomou He <paakmau@outlook.com>
2021-10-06 23:55:05 +0800

    添加了 RequestHeader 与 ResponseHeader

--- include/Protocon/Protocon.h
+++ include/Protocon/Protocon.h
@@ -1,5 +1,23 @@
 #pragma once
 
+#include <cstdint>
+#include <string>
+
 namespace Protocon {
+
+struct RequestHeader {
+    uint16_t commandId;
+    uint64_t gatewayId;
+    uint64_t clientId;
+    uint64_t time;
+    uint16_t apiVersion;
+    uint16_t type;
+};
+
+struct ResponseHeader {
+    uint16_t commandId;
+    uint64_t time;
+    uint8_t status;
+};
 
 }  // namespace Protocon

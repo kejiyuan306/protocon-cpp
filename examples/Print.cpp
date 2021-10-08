3de20fb4705a2f6ffaf77be46630088b2d4c0523
Bomou He <paakmau@outlook.com>
2021-10-08 22:16:07 +0800

    添加 Print 示例

--- examples/Print.cpp
+++ examples/Print.cpp
@@ -2,6 +2,29 @@
 
 #include <cstdio>
 
+bool stop_flag = false;
+
+class PrintResponseHandler : public Protocon::ResponseHandler {
+  public:
+    virtual uint16_t type() { return 0x0001; }
+    virtual void handle(const Protocon::ReceivedResponse& response) {
+        std::printf("Response received, data: %s\n", reinterpret_cast<const char*>(response.data.data()));
+        stop_flag = true;
+    }
+};
+
 int main() {
-    printf("Hello world!");
+    auto client = Protocon::ClientBuilder(2).withResponseHandler(std::make_unique<PrintResponseHandler>()).build();
+
+    client.run("127.0.0.1", 8081);
+
+    client.send(Protocon::SentRequest{
+        .clientId = 0x0001,
+        .type = 0x0001,
+        .data = u8"{\"msg\": \"Hello world!\"}",
+    });
+
+    while (!stop_flag) {
+        client.poll();
+    }
 }

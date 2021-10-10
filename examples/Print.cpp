e0747cd09c3ec9170fd181b11d52947eea37eed5
Bomou He <paakmau@outlook.com>
2021-10-10 16:18:40 +0800

    Print 示例添加注释

--- examples/Print.cpp
+++ examples/Print.cpp
@@ -7,6 +7,7 @@
 bool stop_flag = false;
 
 int main() {
+    // 添加 0x0001 的服务端请求处理器
     auto client =
         Protocon::ClientBuilder(2)
             .withRequestHandler(std::make_pair(0x0001, [](const Protocon::ReceivedRequest& r) {
@@ -20,8 +21,10 @@ int main() {
             }))
             .build();
 
+    // 尝试连接到服务端
     if (!client.run("127.0.0.1", 8081)) return 1;
 
+    // 发送一个 0x0001 的客户端请求，并传入回调函数
     client.send(Protocon::SentRequest{
                     .clientId = 0x0001,
                     .type = 0x0001,
@@ -33,6 +36,8 @@ int main() {
                 });
 
     while (!stop_flag) {
+        // 从服务端收到的报文会放在队列中
+        // 只有在 poll 的时候才会调用各个回调处理相应的报文
         client.poll();
 
         std::this_thread::sleep_for(std::chrono::milliseconds(200));

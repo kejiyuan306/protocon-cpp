374751162158188940f6e06f7a76b146f8689aaf
Bomou He <paakmau@outlook.com>
2021-12-13 19:12:32 +0800

    Add spdlog for logging

--- xmake.lua
+++ xmake.lua
@@ -3,6 +3,7 @@ set_version("2.4.0", {build = "%Y%m%d%H%M"})
 
 set_xmakever("2.5.9")
 
+add_requires("spdlog v1.9.2")
 add_requires("asio 1.20.0")
 
 set_warnings("all", "error")
@@ -12,6 +13,7 @@ target("Protocon")
     set_kind("static")
     add_files("src/*.cpp")
     add_includedirs("include", { public = true })
+    add_packages("spdlog")
     add_packages("asio")
     add_headerfiles("include/(Protocon/*.h)")
 

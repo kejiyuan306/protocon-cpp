7fc3697012aa77e9c5022d145f14a60a3dde3919
Bomou He <paakmau@outlook.com>
2022-01-19 17:00:12 +0800

    Fix syntax errors

--- tests/xmake.lua
+++ tests/xmake.lua
@@ -2,7 +2,7 @@ add_requires("gtest 1.11.0")
 
 target("Tests")
     set_kind("binary")
-    set_default("false")
+    set_default(false)
     add_deps("Protocon")
     add_files("*.cpp")
     add_packages("gtest")
